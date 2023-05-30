0306247734dbb4d17e90f907a2650134c7f486ad
Zanelato <pedeozanelato19@gmail.com>
2023-05-29 20:56:12 -0400

    upgrade structure

--- trab.c
+++ trab.c
@@ -1,12 +1,15 @@
-// #include "trab.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <stdbool.h>
 
-bool r_instru(int32_t instruction) {
-    int8_t rs, rt, rd, shamt, funct, opcode;
+typedef int8_t byte;
+typedef int32_t word;
+typedef int16_t immediate;
+
+bool r_instru(word instruction) {
+    byte rs, rt, rd, shamt, funct, opcode;
     opcode = (instruction & (0x3f << 26)) >> 26;
     rs = (instruction & (0x1f << 21)) >> 21;
     rt = (instruction & (0x1f << 16)) >> 16;
@@ -75,19 +78,82 @@ bool r_instru(int32_t instruction) {
     }
 }
 
-bool j_instru(int32_t instruction) {
+bool j_instru(word instruction) {
+    byte opcode;
+    word address;
+    opcode = (instruction & (0x3f << 26)) >> 26;
+    address = instruction & 0x3ffffff;
+
+    printf("%d, %d\n", opcode, address);
+
+    switch (opcode) {
+        case 0x2: // j - jump
+            printf("j(%d) %d\n", opcode, address);
+            return false;
+        case 0x3: // jal - jump and link
+            printf("jal(%d) %d\n", opcode, address);
+            return false;
+    }
 }
 
-bool i_instru(int32_t instruction) {
+bool i_instru(word instruction) {
+    byte rs, rt, opcode, imm;
+    opcode = (instruction & (0x3f << 26)) >> 26;
+    rs = (instruction & (0x1f << 21)) >> 21;
+    rt = (instruction & (0x1f << 16)) >> 16;
+    imm = instruction & 0xffff;
 
+    switch (opcode) {
+        case 0x4: // beq - branch on equal
+            printf("beq(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x5: // bnq - branch on not equal
+            printf("bnq(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x8: // addi - add immediate
+            printf("addi(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x3: // addiu - add immediate unsigned
+            printf("addiu(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0xa: // slti - set less than immediate
+            printf("slti(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0xc: // andi - and immediate
+            printf("andi(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0xd: // ori - or immediate
+            printf("ori(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0xf: // liu - load upper immediate
+            printf("liu(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x20: // lb - load byte
+            printf("lb(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x21: // lh - load halfword
+            printf("lh(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x23: // lw - load word
+            printf("lw(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x28: // sb - store byte
+            printf("sb(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x29: // sh - store halfword
+            printf("sh(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+        case 0x2b: // sw - store word
+            printf("sw(%d) %d %d %d\n", opcode, rs, rt, imm);
+            return false;
+    }
 }
 
 int main()
 {
     FILE *fp = fopen("exemplo_text.bin", "rb");
-    int32_t instrucao = 0;
-    int32_t opcode, rs, rt, rd, shamt, funct = 0;
-    int16_t imm = 0;
+    word instrucao = 0;
+    word opcode;
 
     if (fp == NULL)
     {
@@ -123,9 +189,9 @@ int main()
         case 0xc: // andi - and immediate
         case 0xd: // ori - or immediate
         case 0xf: // lui - load upper imm.
+        case 0x20: // lb - load byte
+        case 0x21: // lh - load halfword
         case 0x23: // lw - load word
-        case 0x24: // lbu - load byte unsigned
-        case 0x25: // lhu - load halfword unsigned
         case 0x28: // sb - store byte
         case 0x29: // sh - store halfword
         case 0x2b: // sw - store word
@@ -134,10 +200,6 @@ int main()
             }
         default:
             return false;
-        }
-        if (opcode == 8)
-        {
-            printf("addi(%d) %d, %d, %d\n", opcode, rs, rt, imm);
         }
     }
 
